23492720b2980852b4c8fef412580518fa3687d0
Ian Simpson <ianbsimpson@gmail.com>
2022-10-18 12:43:10 -0700

    first commit

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your Name?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            var favoriteColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            var favoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite Band?");
+            var favoriteBand = Console.ReadLine();
+            Console.WriteLine($"Your name is {name}. Your favorite color is {favoriteColor}. Your favorite animal is {favoriteAnimal}. Your favorite band is {favoriteBand}.");
+            
+             
+
         }
     }
 }
